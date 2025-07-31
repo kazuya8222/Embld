ce61ffa6d7340591f1e9764bc6abf1f95e4f4e47
kazuya8222 <pontas0523@gmail.com>
2025-07-31 22:18:12 +0900

    fix:UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,15 +34,7 @@ export default function RootLayout({
     <html lang="ja" className={`${inter.variable} ${notoSansJP.variable}`}>
       <body className="font-sans bg-white">
         <AuthProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navigation />
-            <main className="flex-1 bg-gray-50">
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                {children}
-              </div>
-            </main>
-            <Footer />
-          </div>
+          {children}
           <WelcomeModal />
         </AuthProvider>
       </body>
