5b23ff9a57726d59c453d5289dcf3405bec1815c
kazuya8222 <kazuta.tka0523@gmail.com>
2025-08-23 22:59:57 +0900

    Revert "fix:UI"
    This reverts commit 330b7ff3da6badfc7311e3747800083981260974.

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,32 +1,11 @@
-'use client'
-
 import { Navigation } from '@/components/common/Navigation'
 import { Footer } from '@/components/common/Footer'
-import { useState, useEffect } from 'react'
 
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const [showLayout, setShowLayout] = useState(true)
-
-  useEffect(() => {
-    const handleLayoutToggle = (event: CustomEvent) => {
-      setShowLayout(event.detail.show)
-    }
-
-    window.addEventListener('toggleHomeLayout' as any, handleLayoutToggle)
-    
-    return () => {
-      window.removeEventListener('toggleHomeLayout' as any, handleLayoutToggle)
-    }
-  }, [])
-
-  if (!showLayout) {
-    return <>{children}</>
-  }
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
